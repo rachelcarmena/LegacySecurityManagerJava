da6433f9ef146a08727e0184db962ce9fc78858a
Rachel <raquel.moreno.carmena@gmail.com>
2017-09-01 17:27:00 +0200

    Adding test with boundary condition

--- src/test/java/SecurityManagerShould.java
+++ src/test/java/SecurityManagerShould.java
@@ -43,7 +43,21 @@ public class SecurityManagerShould {
     }
 
     @Test
-    public void create_a_user_if_passwords_match() throws IOException {
+    public void create_a_user_if_passwords_match_and_have_eight_characters() throws IOException {
+        SecurityManager.console = console;
+
+        when(console.readLine()).thenReturn("Rachel", "Rachel M.", "01234567", "01234567");
+        SecurityManager.createUser();
+
+        verify(console).print("Enter a username");
+        verify(console).print("Enter your full name");
+        verify(console).print("Enter your password");
+        verify(console).print("Re-enter your password");
+        verify(console).print("Saving Details for User (Rachel, Rachel M., 76543210)\n");
+    }
+
+    @Test
+    public void create_a_user_if_passwords_match_and_have_more_than_eight_characters() throws IOException {
         SecurityManager.console = console;
 
         when(console.readLine()).thenReturn("Rachel", "Rachel M.", "0123456789", "0123456789");
